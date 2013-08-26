a890ed67add69756e0f4118fd509d81bdaa8d318
Calle Sg <calle@lastname.se>
2013-08-26 21:07:04 +0200

    First commit

---
+++ dnsProxyClient.js
@@ -0,0 +1,257 @@
+
+var dns = require('native-dns'),
+  stdio = require('stdio');
+
+var Nibbler = require('./Nibbler.js').Nibbler;
+
+var options = stdio.getopt({
+    'dnsname': {key: 'd', args: 1, description: 'The dns name of the dns server example: dns.example.com', mandatory: true},
+    'service': {key: 's', args: 1, description: 'The service that we want to connect to.', mandatory: true},
+    'resolver': {key: 'r',args:1, description: 'The ip number of the resolver server we want to use, default is 127.0.0.1'},
+    'port': {key: 'p', args: 1, description: 'The resolver server port the default is 53'},
+    'timing': {key: 't', args: 1, description: 'How often to do dns requests in ms. 500 is default'},
+    'UseDualQuestion': {key: 'q', description: 'Use two questions per request. Some dns servers dont allow that. however if it is supported one can double the up bandwith'},
+    'verbose': {key: 'v', description: 'Print more information to stderr'}
+});
+
+if(!options.resolver){
+    options.resolver = '127.0.0.1';
+}
+if(!options.timing){
+    options.timing = 500;
+}
+if(!options.port){
+    options.port = 53;
+}
+
+var ResolveServer = options.resolver;
+var ResolveServerPort = options.port;
+var ServiceAlias = options.service;
+var ProxyOwner = options.dnsname;
+
+
+var RequestCounter = 0;
+var UpdataID = 0;
+
+var base32 = new Nibbler({
+    dataBits: 8,
+    codeBits: 5,
+    keyString: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789',
+    pad: '',
+    arrayData: true
+});
+
+var Numbase32 = new Nibbler({
+    dataBits: 20,
+    codeBits: 5,
+    keyString: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789',
+    pad: '',
+    arrayData: true
+});
+
+var DownData = [];
+var FinishedDownData = [];
+var NextDownDataID = 0;
+
+//Create A random Number that will be the Id of our session
+var ConnectionIDNum = Math.round(Math.random()*100000);
+var SubmitedTotData = 0;
+
+var AppendStr = '.'+ServiceAlias+'.'+ProxyOwner
+var RequestQue = [];
+
+//Handle Input from the ssh client
+process.openStdin();
+process.stdin.resume();
+
+
+//Force Raw mode i someone might wan't this
+//require('tty').setRawMode(true);
+
+
+//When we get data from the ssh Client
+process.stdin.on('data', function (chunk) {
+	SubmitBuffer(chunk);
+});
+
+process.stdin.on('end', function () {
+  process.exit();
+});
+
+
+setInterval(HandleQue, options.timing);
+
+HandleQue();
+function AddToQue(req1, req2){
+    RequestQue.push([req1, req2])
+}
+
+function HandleQue(){
+    if(RequestQue.length == 0){
+        doDnsRequest();
+    }else{
+        var nextRequest = RequestQue.shift()
+        doDnsRequest(nextRequest[0], nextRequest[1]);
+    }
+}
+
+function SubmitBuffer(SendBuffer){
+	
+	//Convert The Buffer to Base32 text
+	var DataB = base32.encode(SendBuffer);
+	var DataTosend = DataB.length;
+	/*
+    A DNS name can be at max 253 (some places say 255 but i think they count with the start and stop bytes) bytes long and
+    each subdomain maximumly 63 bytes which means that we need to insert 4 dots thats where 249 comes from
+    */
+	var MaxDataBytesInTxt = 249;
+	
+	while(DataB.length != 0){
+		var encodedLength = Numbase32.encode([DataB.length, DataTosend]);
+		var PacketData = Numbase32.encode([UpdataID, ConnectionIDNum, SubmitedTotData]);
+		var QustDataOrg = encodedLength+'.'+PacketData+AppendStr;
+		
+		var Bytes2Use = Math.min(DataB.length, Math.abs(Math.floor(MaxDataBytesInTxt-QustDataOrg.length)));
+		
+		var ActualData = DataB.substr(0, Bytes2Use);
+		DataB = DataB.substr(Bytes2Use);
+		
+		
+		var FormatedData = '';
+		while(ActualData.length > 63){
+			FormatedData += ActualData.substr(0,63) + ".";
+			ActualData = ActualData.substr(63)
+		}
+		FormatedData += ActualData + ".";
+		var QustData = FormatedData + QustDataOrg;
+		
+		
+		var QsDat2;
+
+		//If We should use the secound query
+		if(options.UseDualQuestion && DataB.length != 0){
+			encodedLength = Numbase32.encode([DataB.length, DataTosend]);
+		    PacketData = Numbase32.encode([UpdataID,ConnectionIDNum, SubmitedTotData]);
+			QustDataOrg = encodedLength+'.'+PacketData+AppendStr;
+			Bytes2Use = Math.min(DataB.length, Math.abs(Math.floor(MaxDataBytesInTxt-QustDataOrg.length)));
+			
+			ActualData = DataB.substr(0, Bytes2Use);
+			DataB = DataB.substr(Bytes2Use);
+			
+			FormatedData = '';
+			while(ActualData.length > 63){
+				FormatedData += ActualData.substr(0,63) + ".";
+				ActualData = ActualData.substr(63)
+			}
+			FormatedData += ActualData + ".";
+			QsDat2 = FormatedData + QustDataOrg;
+		}
+		AddToQue(QustData,QsDat2);
+		
+	}
+	SubmitedTotData += DataTosend;
+	UpdataID++;
+}
+
+function doDnsRequest(QustData,SecQuestData){
+	
+	if(typeof(QustData) == 'undefined'){
+		var PacketData = Numbase32.encode([RequestCounter, ConnectionIDNum, SubmitedTotData]);
+		QustData = PacketData+'.'+ServiceAlias+'.'+ProxyOwner;
+	}
+	
+	
+	//console.error("OK", QustData, SecQuestData);
+	
+	var question = dns.Question({
+	  name: QustData,
+	  type: 'TXT',
+	  class: 1
+	});
+	
+	var req = dns.Request({
+	  question: question,
+	  server: { address: ResolveServer, port: ResolveServerPort, type: 'udp' },
+	  cache: false,
+	  timeout: 7000
+	});
+	
+	if(typeof(SecQuestData) != 'undefined'){
+		var question2 = dns.Question({
+	  		name: SecQuestData,
+	  		type: 'TXT',
+	  		class: 1
+			});
+		req.questions = [req.question,question2];
+	}
+	
+	req.on('timeout', function () {
+	  console.error('Timeout in making request');
+	  console.error("ERROR", QustData, SecQuestData);
+	});
+	
+	req.on('message', function (err, answer) {//err should be null
+		if(err != null)
+			console.error("Got an error:",err);
+	  
+	  answer.answer.forEach(function (a) {
+		//console.error("Got Answer:",a.name);
+		if(a.type == 16){
+			
+			var Splitpos = a.data.indexOf(':');
+			if(Splitpos == -1)
+				console.error("could not find split pos ERROR");
+			var Parts = a.data.substr(0,Splitpos);
+			a.data = a.data.substr(Splitpos+1);
+			Parts = Parts.split(".");
+			
+			if(typeof(Parts[2]) != 'undefined' && ConnectionIDNum == Parts[2]){
+				if(typeof(DownData[Parts[1]]) == 'undefined'){
+					DownData[Parts[1]] = []
+				}
+                if(typeof(DownData[Parts[1]][Parts[3]-Parts[0]]) != 'undefined'){
+				    console.error("ERROR got back duplicates of a packet part");
+                }
+                DownData[Parts[1]][Parts[3]-Parts[0]] = a.data;
+			    
+                var DataInSoFar = DownData[Parts[1]].join('');
+                //If we are expecting more data
+				if(Parts[3] != DataInSoFar.length){
+                    HandleQue();
+					//doDnsRequest();
+				}else{
+					//process.stderr.write(DownData[Parts[1]], 'base64');
+                    FinishedDownData[Parts[1]] = Parts[1];
+					for(key in FinishedDownData){
+                        var dwid = FinishedDownData[key];
+                        if(dwid == NextDownDataID){
+				            //console.error("ERROR There is Down data ealier than",Parts[1],"in the que:",key,DownData[key] );
+					        process.stdout.write(DownData[dwid].join(''), 'base64');
+					        delete DownData[dwid];
+					        delete FinishedDownData[key];
+                            NextDownDataID += 1;
+                        }
+                    }
+				}
+			}else{
+				console.error("ERROR got back the following answer:", a.name);
+			}
+		}else{
+			console.error("Could not contact proxy server got back the following answer:",a.name);
+		}
+	  });
+	});
+	
+	
+	req.on('end', function () {
+	
+	 //console.error("End1");
+	});
+	
+	req.send();
+	RequestCounter++;
+
+}
+
+
+
---
+++ dnsProxyServer.js
@@ -0,0 +1,331 @@
+var dns = require('native-dns'),
+  stdio = require('stdio'),
+  server = dns.createServer(),
+  tcpserver,
+  net = require('net'),
+  Nibbler = require('./Nibbler.js').Nibbler;
+
+var options = stdio.getopt({
+    'dnsname': {key: 'd', args: 1, description: 'The dns name of the dns server example: dns.example.com', mandatory: true},
+    'listenip': {key: 'l',args:1, description: 'The ip number to start listening to default is 0.0.0.0'},
+    'port': {key: 'p', args: 1, description: 'The port to listen to default i 53'},
+    'verbose': {key: 'v', description: 'Print more information to stderr'}
+});
+
+if(!options.listenip){
+    options.listenip = '0.0.0.0';
+}
+if(!options.port){
+    options.port = 53;
+}
+
+
+var base32 = new Nibbler({
+    dataBits: 8,
+    codeBits: 5,
+    keyString: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789',
+    pad: '',
+    arrayData: true
+});
+
+
+var BeVerbose = options.verbose;
+
+
+
+var Numbase32 = new Nibbler({
+    dataBits: 20,
+    codeBits: 5,
+    keyString: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789',
+    pad: '',
+    arrayData: true
+});
+
+var ProxyOwner = "."+options.dnsname;
+
+var Services = {
+	s:{host:'localhost',port:1337}
+}
+
+
+var ConnectionPool = [];
+  
+var UseTcp = false;
+if(UseTcp){
+	tcpserver = dns.createTCPServer();
+}
+
+var onMessage = function (request, response) {
+  var i;
+  
+  if(BeVerbose)
+ 	 console.error("# Got query from: "+request._socket._remote.address+" with: "+request.question.length+" question(s)")
+  //Do this once per dns question
+  for(x in request.question){
+    
+    //A question to one of the services that we support should look somthing
+    //like: base32data.base32data.MetaData.PacketData.serviceID.dnsproxy.example.com
+    var QuestionName = request.question[x].name;
+  	var IsToUs = QuestionName.lastIndexOf(ProxyOwner);
+
+    //Does the question end with our Special Domain?
+  	if(IsToUs != -1 && IsToUs == (QuestionName.length - ProxyOwner.length)){
+  		
+  		//Split the question in to its subdomains
+  		var SubDomains = QuestionName.substr(0,IsToUs).split('.');
+  		
+  		//is the question intended for one of the services we support
+	  	if(SubDomains.length > 1){
+			var ServiceID = SubDomains[SubDomains.length-1];
+			if(typeof(Services[ServiceID]) != 'undefined'){
+  			
+	  			//which Socket we wan't 
+	  			var PacketData = Numbase32.decode(SubDomains[SubDomains.length-2]);
+	            if(PacketData.length != 3){
+	                console.error('	Data recived from client is corrupt.');
+	            }else{
+	  			    var cPoolId = PacketData[1];
+		  			var answercounter = 0;
+					console.error('	question regarding session: '+cPoolId);
+  				
+		  			//If The ConnectionPoolID was unknown we create a connection with that id
+		  			if(typeof(ConnectionPool[cPoolId]) == 'undefined'){
+					    if(BeVerbose)
+							console.error("	this is a new session to service "+ServiceID+" establishing new connection to service server");
+						
+		  				ConnectionPool[cPoolId] = {'TotalSentToClient':0, 'TotalRecivedFromClient':0, 'data':[], 'datalen':[], 'socket': null, 'updata':[],'ServiceID':ServiceID,'DowndataID':0};
+		  				ConnectionPool[cPoolId].socket = net.connect(Services[ServiceID].port, Services[ServiceID].host,
+		                    function(){
+		                        console.error(cPoolId+ ' Connected to Service server with ServiceID: '+ConnectionPool[cPoolId].ServiceID);
+		                });
+                	
+		                ConnectionPool[cPoolId].socket.on('data', function(d) {
+		                	console.error(cPoolId+' Got packet with '+d.length+' bytes from Service server with ServiceID: '+ConnectionPool[cPoolId].ServiceID);
+		                	ConnectionPool[cPoolId].datalen.push(d.length);
+		                	ConnectionPool[cPoolId].data.push(d);
+		                });
+                	
+		                ConnectionPool[cPoolId].socket.on('error', function(err) {
+		        			console.error(cPoolId+ " Got Error From Service server Connection", err);
+		    			});
+		    			ConnectionPool[cPoolId].socket.on('close', function() {
+		        			console.error(cPoolId+ ' Service server with ServiceID: '+ConnectionPool[cPoolId].ServiceID+'  disconnected');
+		        		});
+                	
+		  			}else{//The Connection Already exists in the connection pool
+  					
+		  				/*
+		  				############################################################
+		  				Handle upData from The client
+		  				############################################################
+		  				*/
+		  				if(SubDomains.length > 2){//When there is updata there is more than 2 extra subdomains
+  						
+  						
+		  					//A subdomain can only be 63 bytes long so the data is splited in to several subdomains
+		  					var Bs32Data = '';
+		  					for(var i = 0;i<SubDomains.length-3;i++){
+		  						Bs32Data += SubDomains[i];
+		  					}
+  					
+		  					var MetaData = Numbase32.decode(SubDomains[SubDomains.length-3]);
+		  					var DataLeft = MetaData[0];
+		  					var TotalLength = MetaData[1];
+                    
+		                    //var PacketData = Numbase32.decode(SubDomains[SubDomains.length-3]);
+		                    var UpdataID = PacketData[0];
+  					
+		  					//We buffer the recived data until we have the entire tcp packet
+		  					if(typeof(ConnectionPool[cPoolId].updata[UpdataID]) == 'undefined'){
+		  						ConnectionPool[cPoolId].updata[UpdataID] = [];
+		  					}
+		  					if(typeof(ConnectionPool[cPoolId].updata[UpdataID][TotalLength-DataLeft]) == 'undefined'){
+		  						ConnectionPool[cPoolId].updata[UpdataID][TotalLength-DataLeft] = Bs32Data;
+		  					}else{
+		  						console.error('	Got data at offset: '+ (TotalLength-DataLeft)+' More than one time');
+		  					}
+  							var RecivedInQuestion = ((Bs32Data.length/8.0)*5.0);
+		  					var DatRecived = 0.0;//DataLeft - Bs32Data.length;
+							var BytesRecvied = 0.0;
+		  					//When We recive the last part send it away
+		  					for(DatOffset in ConnectionPool[cPoolId].updata[UpdataID]){
+		  						DatRecived += ConnectionPool[cPoolId].updata[UpdataID][DatOffset].length;
+		  					}
+							var BytesDatRecived = (DatRecived/8.0)*5.0;
+		  					if(DatRecived == TotalLength){
+  							
+		  						var UpData = new Buffer(base32.decode(ConnectionPool[cPoolId].updata[UpdataID].join('')));
+								BytesRecvied = UpData.length;
+								ConnectionPool[cPoolId].TotalRecivedFromClient += BytesRecvied;
+		  						//process.stderr.write(ConnectionPool[cPoolId].updata[UpdataID], 'utf8');
+  						
+		  						delete ConnectionPool[cPoolId].updata[UpdataID];
+		  						//console.error(UpData.toString("base64"))
+		  						console.error('	Recived '+RecivedInQuestion+' bytes Proxyed full packet with '+BytesRecvied+' bytes to server');
+		  						ConnectionPool[cPoolId].socket.write(UpData);
+		  					}else{
+		  						console.error('	Recived '+RecivedInQuestion+' bytes the client will send another '+((DataLeft/8.0)*5.0)+' bytes and we have recived '+BytesDatRecived+" The client has sent: "+(TotalLength-DataLeft)+"  of "+ TotalLength +" bytes in the packet");
+		  					}
+  					
+		  					/*else{
+		  						console.error(cPoolId, "The client has sent its last pice of data but we have not gotten the full tcp packet. We must have missed a dns request or gotten it in wrong order");
+		  						console.error(ConnectionPool[cPoolId].updata[UpdataID].length,MetaData);
+		  					}*/
+		  					//}
+  					
+  					
+  				
+		  				}
+  				
+		  				/*
+		  				############################################################
+		  				Submit Data to The client
+		  				############################################################
+		  				*/
+  				
+		  				//A UDP dns answerpacket may not exced 512 bytes
+		  				var RemaningBytes = 500;  //512-12 The static part of a dns response is 12 bytes
+		  				for(qt in response.question){
+  						
+		  					//console.error("Names", response.question[qt].name);
+  						
+		  					RemaningBytes -= response.question[qt].name.length + 6;
+		  				}
+  					
+		  				//console.error("RemaningBytes", RemaningBytes);
+		  				var TotalBytes = 0;
+  					
+  					
+		  				var BytesPerAnswer = 255;//An answer can only hold 255 bytes
+		  				//While we have Data in the buffers and the dns answer is not to long
+		  				while(ConnectionPool[cPoolId].data.length != 0 && RemaningBytes > 0){
+  						
+  						
+		  					//We wait with converting the data to base64 until now to save memory
+		  					if(typeof(ConnectionPool[cPoolId].data[0]) != 'string'){
+		  						ConnectionPool[cPoolId].data[0] = ConnectionPool[cPoolId].data[0].toString('base64')
+                                //console.error('_',ConnectionPool[cPoolId].data[0],'_')
+							    TotalBytes += ConnectionPool[cPoolId].data[0].length;
+		  						ConnectionPool[cPoolId].datalen[0] = ConnectionPool[cPoolId].data[0].length
+		  					}
+  						
+  						
+		  					//Save stuff to put in the answer so that we have it if the current buffer is removed below
+		  					var RemLength = ConnectionPool[cPoolId].data[0].length;
+		  					var UseDownID = ConnectionPool[cPoolId].DowndataID;
+                            var PacketLen = ConnectionPool[cPoolId].datalen[0];
+  						
+		  					var DatArrs = [];
+  						
+  						
+		  					//Numbase32.encode([Math.round(Math.random()*100000)]);
+		  					var NameToUse = response.question[x].name;
+		  					var MetaToClient = RemLength+"."+UseDownID+"."+cPoolId+'.'+PacketLen+':';
+  						
+		  					var BytesInAnswer = Math.min(BytesPerAnswer,RemaningBytes)-NameToUse.length-12-MetaToClient.length;//There is 12 static bytes in a txt record
+  						
+		  					if( 0 < BytesInAnswer){
+		  						//If we have more than one answer can send split the data
+		  						if(ConnectionPool[cPoolId].data[0].length > BytesInAnswer){
+		  							DatArrs.push(MetaToClient+ConnectionPool[cPoolId].data[0].substr(0,BytesInAnswer));
+		  							ConnectionPool[cPoolId].data[0] = ConnectionPool[cPoolId].data[0].substr(BytesInAnswer);
+		  						}else{
+		  							DatArrs.push(MetaToClient+ConnectionPool[cPoolId].data.shift());
+                                    ConnectionPool[cPoolId].datalen.shift();
+		  							ConnectionPool[cPoolId].DowndataID++;
+		  						}
+  					
+  					
+		  						//The loop is unnececary
+		  						for(DPA in DatArrs){
+		  							//console.error('Answer',DatArrs[DPA]);
+		  							RemaningBytes -= DatArrs[DPA].length+NameToUse.length+12;//A answer costs some static bytes
+		  							response.answer.push(dns.TXT({
+		  								name: NameToUse,
+		    							data: DatArrs[DPA],
+		    							ttl: 1
+		  							}));
+		  							answercounter++;
+		  						}
+		  					}else{
+		  						break;
+		  					}
+		  				}
+						if(TotalBytes != 0){
+							console.error("	Sent "+TotalBytes+" bytes to Client");
+							ConnectionPool[cPoolId].TotalSentToClient += TotalBytes;
+						}
+		  			}
+	            }
+	  		}else{
+  	            console.error("Client ask for a service that we dont support:",request.question[x].name);
+	  			/*
+	  			response.answer.push(dns.TXT({
+	    			name: "Unknown.service.alias",
+	    			data: '127.0.0.2',
+	    			ttl: 1,
+	  			}));*/
+	  		}
+		}else{
+  	        console.error("Question does not apper to be corectly formated:",request.question[x].name);
+	  		/*
+	  		response.answer.push(dns.TXT({
+	    		name: "to.few.subdomain.entrys",
+	    		data: '127.0.0.2',
+	    		ttl: 1,
+	  		}));*/
+	  	}
+  	}else{
+  	    console.error("Question not for us:",request.question[x].name);
+  	/*
+  		response.answer.push(dns.TXT({
+    		name: "Unknown.main.domain",
+    		data: '127.0.0.1',
+    		ttl: 1,
+  		}));
+  		*/
+  	}
+  }
+  
+  //To reduce Over head Causes errors in most dns implementations
+  //response.question = [ { name: ProxyOwner, type: 16, class: 1 } ];
+  //console.error('Submit Response');
+  response.send();
+  if(BeVerbose)
+ 	 console.error("__________________________________________")
+};
+
+var onError = function (err, buff, req, res) {
+  console.error('ERROR:' ,err.stack);
+};
+
+var onListening = function () {
+  //console.log('server listening on', this.address());
+  //this.close();
+};
+
+var onSocketError = function (err, socket) {
+  console.log(err);
+};
+
+var onClose = function () {
+  console.log('server closed', this.address());
+};
+
+server.on('request', onMessage);
+server.on('error', onError);
+server.on('listening', onListening);
+server.on('socketError', onSocketError);
+server.on('close', onClose);
+
+server.serve(options.port, options.listenip);
+
+if(UseTcp){
+	tcpserver.on('request', onMessage);
+	tcpserver.on('error', onError);
+	tcpserver.on('listening', onListening);
+	tcpserver.on('socketError', onSocketError);
+	tcpserver.on('close', onClose);
+
+	tcpserver.serve(options.port, options.listenip);
+}
